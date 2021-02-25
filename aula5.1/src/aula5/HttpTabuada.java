bdd0cf3bbf78cf10ff8eb9c93e3a06c1b7877f71
FlavioRbs1 <flaviorbs3@gmail.com>
2021-02-24 22:57:53 -0300

    Fist commit

---
+++ aula5.1/src/aula5/HttpTabuada.java
@@ -0,0 +1,29 @@
+package aula5;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/tabuada")
+public class HttpTabuada extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException{
+		String strNum = req.getParameter("NUMERO");
+		int numero = 7;
+		if(strNum != null) {
+			numero = Integer.parseInt(strNum);
+						
+		}
+		PrintWriter out = res.getWriter();
+		res.setContentType("text/html");
+		
+		for (int i = 0; i<= 10; i++) {
+			int resultado = numero * i;
+			out.printf("<h2>%d X  %d = %d<h2>", numero,i,resultado);
+			
+		}
+	}
+}
